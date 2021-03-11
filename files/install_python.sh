4dc3dd75b1dd6f54958e523153b66e2563fd2f8c
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-03-11 04:48:17 +0100

    Use newer python version

--- files/install_python.sh
+++ files/install_python.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-set -e
+set -euo pipefail
 
 
 if [[ -d "$PYTHON_DIR" ]]; then
@@ -10,14 +10,15 @@ mkdir -p "$PYTHON_DIR"
 cd "$PYTHON_DIR"
 
 
-pypyFile="pypy$PYTHON_VERSION-$PYPY_VERSION-linux_x86_64-portable"
+pypyFile="pypy${PYTHON_VERSION}-v${PYPY_VERSION}-linux64"
+pypyUrl="https://downloads.python.org/pypy/${pypyFile}.tar.bz2"
 tarFile="$PYTHON_DIR/$pypyFile.tar.bz2"
 
 if [[ -e "$tarFile" ]]; then
   tar -xjf "$tarFile"
   rm -rf "$tarFile"
 else
-  wget -O - "https://bitbucket.org/squeaky/portable-pypy/downloads/$pypyFile.tar.bz2" | tar -xjf -
+  wget -O - "$pypyUrl" | tar -xjf -
 fi
 
 mv -n "$pypyFile" pypy
@@ -27,11 +28,14 @@ mkdir -p "$PYTHON_DIR/bin/"
 
 cat > "$PYTHON_DIR/bin/python" <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$PYTHON_DIR/pypy/lib:$LD_LIBRARY_PATH exec $PYTHON_DIR/pypy/bin/pypy "\$@"
+# the substitution below prepends a colon to library path, hence the `:$var` after the plus
+LD_LIBRARY_PATH=$PYTHON_DIR/pypy/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH} exec $PYTHON_DIR/pypy/bin/pypy "\$@"
 EOF
 
 chmod +x "$PYTHON_DIR/bin/python"
 "$PYTHON_DIR/bin/python" --version
+ln -s "$PYTHON_DIR/bin/python" "$PYTHON_DIR/bin/python3"
+chmod +x "$PYTHON_DIR/bin/python3"
 
-
+find "$PYTHON_DIR/" -iname '.bootstrapped_*' -delete
 touch "$PYTHON_DIR/.bootstrapped_${PYTHON_VERSION}_$PYPY_VERSION"
